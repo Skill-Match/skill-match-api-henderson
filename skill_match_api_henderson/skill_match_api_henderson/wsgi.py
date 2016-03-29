1fa3ad5b9bf0a47a3a41ca96cfea8ddec9141dc4
Peter Flynn <fredoflynn@gmail.com>
2016-03-29 15:30:03 -0700

    initial project setup

---
+++ skill_match_api_henderson/skill_match_api_henderson/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for skill_match_api_henderson project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "skill_match_api_henderson.settings")
+
+application = get_wsgi_application()
