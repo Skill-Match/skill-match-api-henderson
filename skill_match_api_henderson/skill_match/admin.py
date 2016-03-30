589dcefb1de9725ecd8e09ffcb79f0a2777bfebc
Peter Flynn <fredoflynn@gmail.com>
2016-03-30 12:46:21 -0700

    add model admin

--- skill_match_api_henderson/skill_match/admin.py
+++ skill_match_api_henderson/skill_match/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from skill_match_api_henderson.skill_match.models import HendersonPark, Amenity
 
-# Register your models here.
+
+@admin.register(HendersonPark)
+class HendersonParkAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'address', 'url', 'img_url')
+
+
+@admin.register(Amenity)
+class AmenityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
