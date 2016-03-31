c1991689ab01c2a520331704b4a5d08db6f4f80b
Peter Flynn <fredoflynn@gmail.com>
2016-03-30 19:51:31 -0700

    add court and amenity serializers

--- skill_match/serializers.py
+++ skill_match/serializers.py
@@ -11,17 +11,20 @@ from skill_match.models import HendersonPark, Amenity, Court
 class AmenitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Amenity
-        fields = ('id', 'name', 'parks')
+        fields = ('name',)
 
 
 class CourtSerializer(serializers.ModelSerializer):
     class Meta:
         model = Court
-        fields = ('id', 'park', 'sport', 'other', 'img_url')
+        fields = ('sport', 'other', 'img_url')
 
 
 class HendersonParkSerializer(serializers.ModelSerializer):
+    court_set = CourtSerializer(many=True, read_only=True)
+    amenity_set = AmenitySerializer(many=True, read_only=True)
+
     class Meta:
         model = HendersonPark
-        fields = ('name', 'address', 'url', 'img_url')
+        fields = ('id', 'name', 'address', 'url', 'img_url', 'court_set', 'amenity_set')
         read_only_fields = ('name', 'address', 'url', 'img_url')
