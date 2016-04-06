64c1a4bbf298d0099ad8a8c20b92f3548b578484
Peter Flynn <fredoflynn@gmail.com>
2016-04-05 19:57:13 -0700

    pointless comment to test heroku

--- skill_match_api_henderson/heroku-settings.py
+++ skill_match_api_henderson/heroku-settings.py
@@ -8,7 +8,7 @@ INSTALLED_APPS = tuple([app for app in INSTALLED_APPS if app not in BLACKLIST])
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
-# This is added for PostGIS
+# This is added for PostGIS {TEST HEROKU}
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
